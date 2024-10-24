8137947c87085626106fad6489bd4f8dd29667fb
Emily Flicos <ejf61@cam.ac.uk>
2024-10-24 17:10:54 +0100

    Also generates basement topog. Overall slope going correct direction now (flow in +ve x direction)

--- python_scripts/generate_topography.py
+++ python_scripts/generate_topography.py
@@ -10,13 +10,15 @@ def gen_top(top_type,nx,ny,dx,dy,Q_x,Q_y,theta,top_ampl_x,top_wl_x,top_ampl_y,to
     
     if top_type=='flat':
         h_top = np.zeros((nx,ny))
+        h_base = np.ones((nx,ny))
     
     if top_type=='slope_x':
-        h_top = slope_x(nx,ny,dx,dy,Q_x,Q_y,theta)
+        h_top, h_base = slope_x(nx,ny,dx,dy,Q_x,Q_y,theta)
         
     if top_type=='cos':
-        h_top = sinusoidal(nx,ny,dx,dy,Q_x,Q_y,theta,top_ampl_x,top_wl_x,top_ampl_y,top_wl_y)
-    return h_top
+        h_top, h_base = sinusoidal(nx,ny,dx,dy,Q_x,Q_y,theta,top_ampl_x,top_wl_x,top_ampl_y,top_wl_y)
+    
+    return h_top, h_base
 
 def sinusoidal(nx,ny,dx,dy,Q_x,Q_y,theta,top_ampl_x,top_wl_x,top_ampl_y,top_wl_y):
     """
@@ -30,7 +32,7 @@ def sinusoidal(nx,ny,dx,dy,Q_x,Q_y,theta,top_ampl_x,top_wl_x,top_ampl_y,top_wl_y
     y_array = (np.arange(ny) - Q_y) * dy
     
     # Contribution from overall slop
-    h_slope = np.tan(theta) * x_array
+    h_slope = - np.tan(theta) * x_array
     h_slope_array = np.transpose(np.tile(h_slope, (ny,1)))
     
     # # Number of modes in x and y
@@ -48,7 +50,7 @@ def sinusoidal(nx,ny,dx,dy,Q_x,Q_y,theta,top_ampl_x,top_wl_x,top_ampl_y,top_wl_y
     if top_wl_y!=0:
         h_top[:] += top_ampl_y * np.cos(2*np.pi/top_wl_y * y_array - np.pi)
         
-    
+    h_base = h_top + 1
     # for i in range(Nx):
         # h_tmp = top_ampl_x[i] * np.cos(2*np.pi/top_wl_x[i] * x_array - np.pi)
         # h_tmp_array = np.transpose(np.tile(h_tmp, (ny,1)))
@@ -62,7 +64,7 @@ def sinusoidal(nx,ny,dx,dy,Q_x,Q_y,theta,top_ampl_x,top_wl_x,top_ampl_y,top_wl_y
     # h_top[-1] = h_top[-2]
     # h_top[:,0] = h_top[:,1]
     # h_top[:,-1] = h_top[:,-2]
-    return h_top
+    return h_top, h_base
 
 def slope_x(nx,ny,dx,dy,Q_x,Q_y,theta):
     x_array = (np.arange(nx) - Q_x) * dx
@@ -70,4 +72,6 @@ def slope_x(nx,ny,dx,dy,Q_x,Q_y,theta):
     
     h_top_line = np.tan(theta) * x_array
     h_top = np.transpose(np.tile(h_top_line, (ny,1)))
-    return h_top
+    
+    h_base = h_top + 1
+    return h_top, h_top
